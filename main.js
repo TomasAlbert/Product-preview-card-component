ba4a794cadd2aad5cc092ecbf502e8f35ba87e6d
TomasAlbert <tomasalbert9387182@gmail.com>
2023-12-06 16:07:31 +0100

    revert for deploy

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-window.addEventListener("DOMContentLoaded", () => {
-	const html = document.querySelector("html");
-	const card = document.querySelector("[data-card ]");
+// window.addEventListener("DOMContentLoaded", () => {
+// 	const html = document.querySelector("html");
+// 	const card = document.querySelector("[data-card ]");
 
-	if (html) {
-		html.style.transition = "opacity .1s ease-in-out";
-		html.style.opacity = "1";
-	}
-	if (card) {
-		setTimeout(() => {
-			card.classList.add("animate-in");
-		}, 1000);
-	}
-});
+// 	if (html) {
+// 		html.style.transition = "opacity .1s ease-in-out";
+// 		html.style.opacity = "1";
+// 	}
+// 	if (card) {
+// 		setTimeout(() => {
+// 			card.classList.add("animate-in");
+// 		}, 1000);
+// 	}
+// });
