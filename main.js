f0be9ace7a0a3de44d742aba593871494dc42478
TomasAlbert <tomasalbert9387182@gmail.com>
2023-12-06 16:04:32 +0100

    feat: add loading animation

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("hello");
+window.addEventListener("DOMContentLoaded", () => {
+	const html = document.querySelector("html");
+	const card = document.querySelector("[data-card ]");
+
+	if (html) {
+		html.style.transition = "opacity .1s ease-in-out";
+		html.style.opacity = "1";
+	}
+	if (card) {
+		setTimeout(() => {
+			card.classList.add("animate-in");
+		}, 1000);
+	}
+});
